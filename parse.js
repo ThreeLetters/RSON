127976a93cdba7dbf9bea85fde28182763156f93
Andrew54757 <andrews54757@gmail.com>
2016-09-11 13:58:40 -0400

    Update parse.js

--- parse.js
+++ parse.js
@@ -1,1 +1,28 @@
-
+function parse(text) {
+function getBPos(text) {
+    var a = 0;
+    var start = "n";
+    var end = false;
+    var index = 0;
+    var t = 0;
+    for (var i = 0;i<text.length;i++) {
+      a = text.indexOf("{",index)
+      
+      if (a == -1) break;
+        if (start == "n") start = a;
+    index = a + 1;
+          t ++;
+      
+    }
+index = 0;
+    for (var i = 0; i < t; i++) {
+        a = text.indexOf("}",index)
+        
+        if (a == -1) break;
+        index = a + 1;
+    }
+    
+    return {start: start,end: index - 1}
+    
+}
+}
