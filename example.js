30a86b36daf1f2da28a74abbd187d01513b90653
Andrew54757 <andrews54757@gmail.com>
2016-09-12 14:17:48 -0400

    Update example.js

--- example.js
+++ example.js
@@ -29,4 +29,20 @@ var a = RSON.stringify(objects)
 var b = JSON.stringify(objects)
 console.log("Another great feature is its compactness. Look at this sample object")
 console.log(objects)
-console.log("JSON's outputs length is " + b.length + " but RSON's is " + a.length)
+console.log("JSON's outputs length is " + b.length + " but RSON's is " + a.length);
+console.log("RSON can also stringify functions!");
+objects = {
+   name: "RSON",
+   awesomeness: "Better than JSON",
+   anotherfeature: function() {return "Functions!"}
+}
+console.log("RSON can also store functions. Example")
+console.log(objects);
+var a = RSON.stringify(objects);
+console.log("RSON results: " + a);
+console.log("Parsing back:")
+var b = RSON.parse(a);
+console.log(b);
+console.log("Calling the function..");
+console.log(b.anotherfeature())
+
