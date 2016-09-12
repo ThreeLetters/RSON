4d1da575a03f365c1664e8fbcf1c5f8b85dfca4e
Andrew54757 <andrews54757@gmail.com>
2016-09-11 20:50:27 -0400

    Update example.js

--- example.js
+++ example.js
@@ -18,3 +18,14 @@ try {
 var a = RSON.stringify(objects) // RSON can!
 console.log("this is the output for RSON! " + a)
 console.log("RSON can parse it back... " + RSON.parse(a))
+objects = {
+   name: "RSON",
+   awesomeness: "to the sky",
+   creator: "AJS Development",
+   anotherfeature: "Compactness!"
+}
+var a = RSON.stringify(objects)
+var b = JSON.stringify(objects)
+console.log("Another great feature is its compactness. Look at this sample object")
+console.log(objects)
+console.log("JSON's outputs length is " + b.length + " but RSON's is " + a.length
