a311122964b93e943f4c4d52c6c7880054695664
Andrew S <andrews54757@gmail.com>
2016-10-25 17:16:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   stringify: require('./stringify.js'),
   parse: require('./parse.js'),
+  efficient: require('./rson-efficient.js'),
   version: function() {
     return JSON.parse(require('fs').readFileSync('./package.json',"utf8")).version
   }
