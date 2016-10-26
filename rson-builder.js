ec9df898e4d5bfd5e52fff90f112830132c57775
Andrew S <andrews54757@gmail.com>
2016-10-26 13:15:12 -0400

    Update rson-builder.js

--- rson-builder.js
+++ rson-builder.js
@@ -1,1 +1,16 @@
+"use strict"
 
+module.exports = class RSONBuilder {
+  constructor() {
+    this.obj = []
+  }
+  append(a) {
+  
+    this.obj.push(a) 
+  }
+  parse() {
+   return "{" + this.obj.join("|") + "}"
+   
+  }
+  
+}
